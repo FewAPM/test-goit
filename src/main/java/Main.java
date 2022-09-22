ddab3531c8410680a9ae60fb829be9d25b2f8307
Yaroslav Starenkyi <ystarenkiy@gmail.com>
2022-09-22 16:10:19 +0300

    Added  print method

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public void print(){
+        System.out.println("My first method");
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello GIT!");
+        new Main().print();
+    }
+}
