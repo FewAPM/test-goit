c228ec1d02b12f7bcbe29938747885541e68fa7e
Yaroslav Starenkyi <ystarenkiy@gmail.com>
2022-09-22 16:18:45 +0300

    Added print 2 print mothods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,13 @@
 public class Main {
-    public void print(){
+    public void printFirst(){
         System.out.println("My first method");
+    }
+    public void printSecond(){
+        System.out.println("My second method");
     }
     public static void main(String[] args) {
         System.out.println("Hello GIT!");
-        new Main().print();
+        new Main().printFirst();
+        new Main().printSecond();
     }
 }
