f0eaece1f28430a0e755ad156d62eea315fc0fc7
Yaroslav Starenkyi <ystarenkiy@gmail.com>
2022-09-22 23:35:18 +0300

    Began studying Loops
    The first part

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -1,14 +1,45 @@
-public class TestClass {
-    public void print(String s){
-        System.out.println("s");
-    }
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
-    public int sum (int a, int b) {
-        return a + b;
-    }
+public class TestClass {
+
 
     public static void main(String[] args) {
-    new TestClass().print("It is rather difficult for me :(");
-        new TestClass().sum(132156,6655);
+        int row = 4;
+        int column = 3;
+        int counter = 1;
+        int [][] array = new int[row][column];
+
+
+
+
+//        for (int i = 0; i < array.length; i++) {
+//            System.out.println("i " + i);
+//            for (int j = 0; j < array[i].length; j++) {
+//                System.out.println(" j " + j);
+//                array[i][j] = counter++;
+//               // System.out.print(" " + array[i][j]);
+//            }
+//           // System.out.println();
+//        }
+//        System.out.println();
+//        for (int i = 0; i < array.length; i++) {
+//           // System.out.println("i " + i);
+//            for (int j = 0; j < array[i].length; j++) {
+//                //System.out.println("j " + j);
+//                //array[i][j] = counter++;
+//                System.out.print(" " + array[i][j]);
+//            }
+//            System.out.println();
+//        }
+
+        for (int[] element: array) {
+            for (int innerElement: element) {
+                System.out.print(" ");
+                System.out.print( innerElement = counter ++);
+            }
+            System.out.println();
+        }
+
     }
 }
