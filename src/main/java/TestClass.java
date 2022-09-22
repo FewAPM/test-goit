b1873294a2d85f7e0d70e19acd0b048f32772eeb
Yaroslav Starenkyi <ystarenkiy@gmail.com>
2022-09-22 18:00:13 +0300

    Added sum method

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -2,7 +2,13 @@ public class TestClass {
     public void print(String s){
         System.out.println("s");
     }
+
+    public int sum (int a, int b) {
+        return a + b;
+    }
+
     public static void main(String[] args) {
     new TestClass().print("It is rather difficult for me :(");
+        new TestClass().sum(132156,6655);
     }
 }
