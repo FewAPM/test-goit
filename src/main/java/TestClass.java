734b87c6a6a41c05cd42cfbc0aac3217a2f276e5
Yaroslav Starenkyi <ystarenkiy@gmail.com>
2022-09-22 23:42:14 +0300

    Update TestClass.java

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -13,33 +13,33 @@ public class TestClass {
 
 
 
-//        for (int i = 0; i < array.length; i++) {
-//            System.out.println("i " + i);
-//            for (int j = 0; j < array[i].length; j++) {
-//                System.out.println(" j " + j);
-//                array[i][j] = counter++;
-//               // System.out.print(" " + array[i][j]);
-//            }
-//           // System.out.println();
-//        }
-//        System.out.println();
-//        for (int i = 0; i < array.length; i++) {
-//           // System.out.println("i " + i);
-//            for (int j = 0; j < array[i].length; j++) {
-//                //System.out.println("j " + j);
-//                //array[i][j] = counter++;
-//                System.out.print(" " + array[i][j]);
-//            }
-//            System.out.println();
-//        }
-
-        for (int[] element: array) {
-            for (int innerElement: element) {
-                System.out.print(" ");
-                System.out.print( innerElement = counter ++);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("i " + i);
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.println(" j " + j);
+                array[i][j] = counter++;
+               // System.out.print(" " + array[i][j]);
+            }
+           // System.out.println();
+        }
+        System.out.println();
+        for (int i = 0; i < array.length; i++) {
+           // System.out.println("i " + i);
+            for (int j = 0; j < array[i].length; j++) {
+                //System.out.println("j " + j);
+                //array[i][j] = counter++;
+                System.out.print(" " + array[i][j]);
             }
             System.out.println();
         }
+
+//        for (int[] element: array) {
+//            for (int innerElement: element) {
+//                System.out.print(" ");
+//                System.out.print( innerElement = counter ++);
+//            }
+//            System.out.println();
+//        }
 
     }
 }
