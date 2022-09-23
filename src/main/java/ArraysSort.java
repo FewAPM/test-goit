8ccf35ee4dbf998b39a7a4b6abb49f835d6425a7
Yaroslav Starenkyi <ystarenkiy@gmail.com>
2022-09-23 11:36:20 +0300

    Bubble sort

---
+++ src/main/java/ArraysSort.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class ArraysSort {
+    public static void main(String[] args) {
+        int [] arr = {11, 3, 14, 16, 7};
+        int buf;
+        boolean isTrue = false;
+
+        while(!isTrue) {
+            isTrue = true;
+            for (int i = 0; i < arr.length-1; i++) {
+                if (arr[i] > arr[i+1]) {
+                    isTrue = false;
+                    buf = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = buf;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
