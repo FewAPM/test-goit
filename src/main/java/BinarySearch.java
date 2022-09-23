de1986b9955dabd603b47829014f929c86d8a0c7
Yaroslav Starenkyi <ystarenkiy@gmail.com>
2022-09-23 21:56:14 +0300

    created BinarySearch

---
+++ src/main/java/BinarySearch.java
@@ -0,0 +1,34 @@
+public class BinarySearch {
+    static int counter = 0;
+
+    private static int binarySearch(int elementForSearch){
+        int [] array = new int[] { -10, -9, -7, -5,-4,0,1,2,3,4,5,6,10,11,23,25,26,28,29};
+        int lowestIndex = 0;
+        int highestIndex = array.length-1;
+
+
+        while (lowestIndex <= highestIndex) {
+            counter++;
+            int midIndex = (lowestIndex + highestIndex) / 2;
+            int midElement = array[midIndex];
+            if (midElement < elementForSearch) {
+                lowestIndex = midIndex +1;
+
+            } else if (midElement > elementForSearch) {
+                highestIndex = midIndex -1;
+
+            } else {
+                return midElement;
+            }
+
+            }             return -1;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(binarySearch(-10));
+        System.out.println(counter);
+    }
+    }
+
+
---
+++ src/main/java/LinearSearch.java
@@ -0,0 +1,34 @@
+public class LinearSearch {
+    private static int linearSearch(int[] arr, int search) {
+        int [] array = arr;
+        int elemToSearch = search;
+        int resultElem = -1;
+        int counter = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            counter++;
+            if(elemToSearch == array[i]) {
+                resultElem = array[i];
+                break;
+            }
+
+        }
+
+        System.out.println("counter : " + counter);
+        System.out.println("array length: " + array.length);
+        System.out.println("resultElem : " + resultElem);
+        System.out.println("resultElem index in array : " + (counter - 1));
+        return resultElem;
+    }
+    public static void main(String[] args) {
+    new LinearSearch().linearSearch(new int[] { -10, -9, -7, -5,-4,0,1,2,3,4,5,6,10,11,23,25,26,28,29}, 10); // counter 13
+       // System.out.println(linearSearch(new int[] { -10, -8, 6, 5, 3, 0, 1, 5, 7, 25}, 5));
+
+
+
+
+        }
+
+
+}
+
