cd46868e17b1cef4342663c689bbcd1ea3e03c27
Yaroslav Starenkyi <ystarenkiy@gmail.com>
2022-09-23 21:56:39 +0300

    Update TestClass.java

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -1,45 +1,31 @@
-import java.lang.reflect.Array;
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class TestClass {
 
 
     public static void main(String[] args) {
-        int row = 4;
-        int column = 3;
-        int counter = 1;
-        int [][] array = new int[row][column];
+        Scanner scanner = new Scanner(System.in);
+        String country;
 
-
-
-
-        for (int i = 0; i < array.length; i++) {
-            System.out.println("i " + i);
-            for (int j = 0; j < array[i].length; j++) {
-                System.out.println(" j " + j);
-                array[i][j] = counter++;
-               // System.out.print(" " + array[i][j]);
-            }
-           // System.out.println();
-        }
-//        System.out.println();
-//        for (int i = 0; i < array.length; i++) {
-//           // System.out.println("i " + i);
-//            for (int j = 0; j < array[i].length; j++) {
-//                //System.out.println("j " + j);
-//                //array[i][j] = counter++;
-//                System.out.print(" " + array[i][j]);
-//            }
-//            System.out.println();
-//        }
-
-//        for (int[] element: array) {
-//            for (int innerElement: element) {
-//                System.out.print(" ");
-//                System.out.print( innerElement = counter ++);
-//            }
-//            System.out.println();
-//        }
-
+    int i = scanner.nextInt();
+    switch (i){
+        case 1:
+        case 2:
+        case 5:
+            country = "Spain";
+            break;
+        case 3:
+        case 7:
+        country = "Germany";
+            break;
+        case 12:
+        case 15:
+        case 20:
+            country = "GB";
+            break;
+        default:
+            country = "Unknown";
+    }
+        System.out.println(country);
     }
 }
