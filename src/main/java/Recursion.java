fc453fd856da25dfe76e2e8863a429ead538ccfd
Yaroslav Starenkyi <ystarenkiy@gmail.com>
2022-09-23 21:55:42 +0300

    created Recursion

---
+++ src/main/java/Recursion.java
@@ -0,0 +1,13 @@
+public class Recursion {
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+    public static int factorial(int number) {
+        if(number == 0 || number == 1) {
+            return 1;
+        }
+        int result = number * factorial(number-1);
+        return result;
+    }
+
+}
