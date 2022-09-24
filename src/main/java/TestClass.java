b70675f22cdebe93d51977f00c1b8b24a73a70c9
Yaroslav Starenkyi <ystarenkiy@gmail.com>
2022-09-24 18:53:51 +0300

    findMinMaxPrices
    Java Core HW 4
    
    ЗАВДАННЯ 3/17

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -1,37 +1,35 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TestClass {
 
-    public void printPrices(float[] prices) {
-        for (int i = 0; i < prices.length; i++) {
-            System.out.println(prices[i] + " jup.");
-
+    public int[] findMinMaxPrices(int[] prices){
+        int min, max;
+        int [] newArray = new int []{};
+        if (prices.length == 0) {
+            return newArray;
+        } else {
+            int temp;
+            for (int i = 0; i < prices.length-1; i++) {
+                if (prices[i] > prices[i+1]) {
+                    temp = prices[i];
+                    prices[i] = prices[i+1];
+                    prices[i+1] = temp;
+                }
+            }
+            min = prices[0];
+            max = prices[prices.length-1];
+            if (min == max) {
+                return newArray = new int [] {min};
+            }
+            return newArray = new int [] {min, max};
         }
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String country;
 
-    int i = scanner.nextInt();
-    switch (i){
-        case 1:
-        case 2:
-        case 5:
-            country = "Spain";
-            break;
-        case 3:
-        case 7:
-        country = "Germany";
-            break;
-        case 12:
-        case 15:
-        case 20:
-            country = "GB";
-            break;
-        default:
-            country = "Unknown";
-    }
-        System.out.println(country);
+        public static void main(String[] args) {
+            System.out.println(Arrays.toString(new TestClass().findMinMaxPrices(new int[] {})));
+        ;
+
     }
 }
