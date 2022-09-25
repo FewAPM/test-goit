51173e1231e9570b88f50396c2fac6875d210540
Yaroslav Starenkyi <ystarenkiy@gmail.com>
2022-09-25 23:53:13 +0300

    Update DeleteFromArray.java

--- src/main/java/DeleteFromArray.java
+++ src/main/java/DeleteFromArray.java
@@ -21,7 +21,7 @@ public class DeleteFromArray {
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(new TestClass().removePrice(new int[] {6, 0, 5, 9, 5, 7, 9, 0, 7, 0}, 0)));
+       // System.out.println(Arrays.toString(new TestClass().removePrice(new int[] {6, 0, 5, 9, 5, 7, 9, 0, 7, 0}, 0)));
 
     }
 }
