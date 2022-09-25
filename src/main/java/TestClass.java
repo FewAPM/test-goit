2da61bd1a1f1964a255aaedf5972dc47aa080072
Yaroslav Starenkyi <ystarenkiy@gmail.com>
2022-09-25 23:53:20 +0300

    Update TestClass.java

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -6,28 +6,20 @@ import java.util.Scanner;
 
 public class TestClass {
 
-    public int[] removePrice(int[] prices, int toRemove) {
-        int toRemoveCount = 0;
-        for(int price: prices) {
-            if (price == toRemove) {
-                toRemoveCount++;
-            }
-        }
-
-        int[] result = new int[prices.length - toRemoveCount];
-        int index = 0;
-        for (int i = 0; i < prices.length; i++) {
-            if (prices[i] != toRemove) {
-                result[index] = prices[i];
-                index++;
-            }
-        }
-        return result;
-    }
-
         public static void main(String[] args) {
-            System.out.println(Arrays.toString(new TestClass().removePrice(new int[] {6, 0, 5, 9, 5, 7, 9, 0, 7, 0}, 0)));
+           // System.out.println(Arrays.toString(new TestClass().removePrice(new int[] {6, 0, 5, 9, 5, 7, 9, 0, 7, 0}, 0)));
+            int [] arr = new int[100];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = i;
+            }
+            int low = arr[0];
+            int high = arr[arr.length-1];
+            int mid = (low + high) / 2;
+            System.out.println(mid);
+            System.out.println(mid/2);
 
+            boolean b = 20 > 10;
+            System.out.println(b);
             }
     }
 
