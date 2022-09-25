ae9b3adf543701f583322954aa0175d6a06ef54f
Yaroslav Starenkyi <ystarenkiy@gmail.com>
2022-09-25 13:16:18 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     // This is from my PC :)
     public void printFirst(){
@@ -7,8 +9,24 @@ public class Main {
         System.out.println("My second method");
     }
     public static void main(String[] args) {
-        System.out.println("Hello GIT!");
-        new Main().printFirst();
-        new Main().printSecond();
+
+        int [] a = new int[] {1, 99, 5, 49};
+          int  index = 0;
+        for (int element: a) {
+            if ((element % 10) == 9) {
+                index++;
+            }
+        }
+        int k = 0;
+        int [] result = new int[a.length-index];
+        for (int element: a) {
+            if ((element % 10) == 9) {
+                result[k] = element;
+                k++;
+            }
+        }
+
+        System.out.println(Arrays.toString(result));
+
     }
 }
