deed298bcd2382c433fdd8bcf93294fbb530af99
Yaroslav Starenkyi <Ystarenkiy@gmail.com>
2022-09-25 22:23:15 +0300

    mergeStocks
    Java Core HW 4
    
    ЗАВДАННЯ 7/17

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,31 +2,24 @@ import java.util.Arrays;
 
 public class Main {
     // This is from my PC :)
-    public void printFirst(){
-        System.out.println("My first method");
-    }
-    public void printSecond(){
-        System.out.println("My second method");
+    public String[] mergeStocks(String[] showcaseStocks, String[] warehouseStocks) {
+        if (showcaseStocks.length == 0 & warehouseStocks.length == 0) {
+            return new String [0];
+        }
+        int counter = 0;
+        String [] result = new String [showcaseStocks.length + warehouseStocks.length];
+        for (String element: showcaseStocks) {
+            result[counter] = element;
+            counter++;
+        }
+        for (String element: warehouseStocks) {
+            result[counter] = element;
+            counter++;
+        }
+        return result;
     }
     public static void main(String[] args) {
 
-        int [] a = new int[] {1, 99, 5, 49};
-          int  index = 0;
-        for (int element: a) {
-            if ((element % 10) == 9) {
-                index++;
-            }
-        }
-        int k = 0;
-        int [] result = new int[a.length-index];
-        for (int element: a) {
-            if ((element % 10) == 9) {
-                result[k] = element;
-                k++;
-            }
-        }
-
-        System.out.println(Arrays.toString(result));
 
     }
 }
