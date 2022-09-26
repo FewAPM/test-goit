3c903669a18d5f0977e0991d7883371b68fef14a
Yaroslav Starenkyi <ystarenkiy@gmail.com>
2022-09-26 22:18:02 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,8 @@
 import java.util.Arrays;
 
 public class Main {
-    // This is from my PC :)
-    public String[] mergeStocks(String[] showcaseStocks, String[] warehouseStocks) {
-        if (showcaseStocks.length == 0 & warehouseStocks.length == 0) {
-            return new String [0];
-        }
-        int counter = 0;
-        String [] result = new String [showcaseStocks.length + warehouseStocks.length];
-        for (String element: showcaseStocks) {
-            result[counter] = element;
-            counter++;
-        }
-        for (String element: warehouseStocks) {
-            result[counter] = element;
-            counter++;
-        }
-        return result;
-    }
+
+
     public static void main(String[] args) {
 
 
