86aff86eb71e814c6d7a276ceac2ce5babd1779e
Yaroslav Starenkyi <ystarenkiy@gmail.com>
2022-09-28 12:04:47 +0300

    Better version drawingQuad

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -6,20 +6,26 @@ import java.util.Scanner;
 
 public class TestClass {
 
-        public static void main(String[] args) {
-           // System.out.println(Arrays.toString(new TestClass().removePrice(new int[] {6, 0, 5, 9, 5, 7, 9, 0, 7, 0}, 0)));
-            int [] arr = new int[100];
-            for (int i = 0; i < arr.length; i++) {
-                arr[i] = i;
+        public String drawQuad(int n) {
+            int columns = n;
+            String result = "";
+        while (columns > 0) {
+            columns --;
+            int rows = n;
+            while (rows > 0) {
+                result += '*';
+                rows--;
             }
-            int low = arr[0];
-            int high = arr[arr.length-1];
-            int mid = (low + high) / 2;
-            System.out.println(mid);
-            System.out.println(mid/2);
+            result += '\n';
+        }
 
-            boolean b = 20 > 10;
-            System.out.println(b);
-            }
+            return result;
+
+        }
+
+    public static void main(String[] args) {
+        System.out.println(new TestClass().drawQuad(5));
     }
+
+}
 
