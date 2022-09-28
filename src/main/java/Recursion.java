ef680c3c5b3dad9321fc554a83d8aa4f6f44e9aa
Yaroslav Starenkyi <ystarenkiy@gmail.com>
2022-09-28 17:14:02 +0300

    Add method sumQuads
    Java Core HW 4
    ЗАВДАННЯ 15/17

--- src/main/java/Recursion.java
+++ src/main/java/Recursion.java
@@ -1,6 +1,7 @@
 public class Recursion {
     public static void main(String[] args) {
         System.out.println(factorial(5));
+        System.out.println(new Recursion().sumQuads(3));
     }
     public static int factorial(int number) {
         if(number == 0 || number == 1) {
@@ -10,4 +11,16 @@ public class Recursion {
         return result;
     }
 
+    public int sumQuads(int n) {
+        int result  = 0;
+        if(n == 0 || n == 1) {
+            return 1;
+        }
+        while (n > 0) {
+
+            result += (n * n) ;
+            n--;
+        }
+        return result;
+    }
 }
