48c6816cbe575dcc50e9560d9106ac182ba6ac13
Yaroslav Starenkyi <ystarenkiy@gmail.com>
2022-09-28 17:14:07 +0300

    Create TriangleDrawer.java

---
+++ src/main/java/TriangleDrawer.java
@@ -0,0 +1,21 @@
+public class TriangleDrawer {
+
+    public String drawTriangle(int side) {
+        String str = "";
+        int width = side;
+    while (side > 0) {
+    side--;
+    while (width > 0) {
+        str += '*';
+        width--;
+    }
+    width = side;
+    str += '\n';
+    }
+    return str;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new TriangleDrawer().drawTriangle(3));
+    }
+}
