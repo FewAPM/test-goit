9ddca2ae302d6a3aea066f02affbd6abf63901dd
Yaroslav Starenkyi <ystarenkiy@gmail.com>
2022-09-28 12:22:30 +0300

    added new method "drawRect"

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -22,9 +22,24 @@ public class TestClass {
             return result;
 
         }
+    public String drawRect(int width, int height, char c){
+        String result = "";
+        int w = height;
+        while (w > 0) {
+            w--;
+            int h = width;
+            while (h > 0) {
+                h--;
+                result += c;
+            }
+            result += '\n';
+        }
+        return result;
+    }
 
     public static void main(String[] args) {
-        System.out.println(new TestClass().drawQuad(5));
+       // System.out.println(new TestClass().drawQuad(5));
+        System.out.println(new TestClass().drawRect(5, 6, 'X'));
     }
 
 }
